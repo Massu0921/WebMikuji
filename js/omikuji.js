95689cc00c74f0c82e8aaadf62ff6f67ee1f0a5b
Massu <imomushi.pipi@gmail.com>
2018-12-30 01:28:44 +0900

    不要構文消去

--- js/omikuji.js
+++ js/omikuji.js
@@ -5,23 +5,6 @@ window.addEventListener('devicemotion', motion);
 
 // カウント用
 var cnt = 0;
-var cnt_old = 0;
-const cntmax = 128;
-
-// json送信
-function sendjson(cnt){
-  var json = {
-    cnt: cnt
-  };
-
-  $.ajax({
-    url: '/send',
-    type: 'post',
-    data: JSON.stringify(json),
-    contentType: 'application/JSON',
-    dataType: 'JSON'
-  });
-}
 
 // 加速度変化時
 function motion(event) {
@@ -42,12 +25,10 @@ function motion(event) {
   else if (y > bound) { // 上
     omikuji_img.style.transform = 'rotate(0deg)';
     cnt++;
-    sendjson(cnt);
   }
   else if (y < -bound) { // 下
     omikuji_img.style.transform = 'rotate(180deg)';
     cnt++;
-    sendjson(cnt);
   }
   //console.log(cnt)
 }
