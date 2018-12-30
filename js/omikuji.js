41b9742f706616c1c92dea7cd0263a84fbfc3dfe
Massu <imomushi.pipi@gmail.com>
2018-12-30 23:56:24 +0900

    結果画面実装準備

--- js/omikuji.js
+++ js/omikuji.js
@@ -5,6 +5,7 @@ window.addEventListener('devicemotion', motion);
 
 // カウント用
 var cnt = 0;
+const max_cnt = 100;
 
 // 加速度変化時
 function motion(event) {
@@ -30,5 +31,15 @@ function motion(event) {
     omikuji_img.style.transform = 'rotate(180deg)';
     cnt++;
   }
+
+  if (cnt >= max_cnt){
+    // 結果表示
+    result()
+  }
   //console.log(cnt)
+}
+
+// 結果表示用(書き換え)
+function result(){
+  $('txt').text("結果は...?")
 }
